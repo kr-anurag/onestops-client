f1599f93168419af179be590fd1e4429e963c297
Anurag <anurag.kumar@kroger.com>
2025-02-19 15:07:57 +0530

    UI Fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -129,7 +129,7 @@ const Home = () => {
   return (
       <>
         <Box
-            h={"65dvh"}
+            h={"70dvh"}
             bgImage={bannerImage}
             bgPosition="center"
             bgRepeat="no-repeat"
